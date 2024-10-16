80f6fcba774c845f6506602c2618e5debddaab21
Satish-mourya <satishmourya.o970@gmail.com>
2024-10-16 13:06:00 +0530

    bg-changer app

---
+++ tailwind-React/src/components/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Card(){
+    return(
+        <div>
+            <img src="" alt="" />
+            <h1 className="text-2xl bg-green-500 p-3 rounded"> a card for push</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, iusto?</p>
+
+        </div>
+    )
+}
